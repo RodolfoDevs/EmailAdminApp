eaf14cb3b39ecc54daede2aca240840afb641239
rudy1787 <rmadrizcr@gmail.com>
2019-07-22 19:01:03 -0500

    First Commit - Uploading all the files
    Functionality goes up to setting the first name, last name, and the
    department in the constructor. Department is set by asking user to input
    an integer for the department where the new employee will be working.

---
+++ EmailApp/src/emailApp/EmailApp.java
@@ -0,0 +1,12 @@
+package emailApp;
+
+public class EmailApp {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		//Creating an instance of Email to test constructor
+		Email em1 = new Email("John", "Doe");
+	}
+
+}
